d80e653965589f9532f0c15664b8197ad3021e56
shun-cheng <shuncheng.wu91@gmail.com>
2021-09-21 14:49:55 +0200

    support newer version of Eigen library

--- cmake/LinkEigen3.cmake
+++ cmake/LinkEigen3.cmake
@@ -1,7 +1,10 @@
 ####################
 # LinkEigen3.cmake #
 ####################
-
-if(EIGEN3_INCLUDE_DIR)
+if(TARGET Eigen3::Eigen)
+  target_link_libraries(${targetname} PUBLIC Eigen3::Eigen)
+elseif(EIGEN3_INCLUDE_DIR)
   target_include_directories(${targetname} PUBLIC ${EIGEN3_INCLUDE_DIR})
-endif(EIGEN3_INCLUDE_DIR)
+else()
+  MESSAGE(FATAL_ERROR "cannot find eigen3")
+endif()
--- cmake/UseEigen3.cmake
+++ cmake/UseEigen3.cmake
@@ -1,18 +1,21 @@
 ###################
 # UseEigen3.cmake #
 ###################
-find_package(Eigen3 3.3.90 QUIET)
+find_package(Eigen3 3.4 QUIET)
 OPTION(WITH_EIGEN "Build with libEIGEN support?" OFF)
 #MESSAGE("EIGEN!" ${WITH_EIGEN})
 IF(WITH_EIGEN)
-#  find_package(Eigen3)
-#  MESSAGE("USE EIGEN " ${Eigen_FOUND} )
   IF(NOT Eigen3_FOUND)
     INCLUDE(ConfigureTimeDependency)
     add_configure_time_dependency(Eigen3)
-    find_package(Eigen3 REQUIRED)
+    find_package(Eigen3 3.4 REQUIRED)
+  ENDIF()
+
+  IF(TARGET Eigen3::Eigen)
+    GET_TARGET_PROPERTY(EIGEN3_INCLUDE_DIR Eigen3::Eigen INTERFACE_INCLUDE_DIRECTORIES)
   ENDIF()
   include_directories(${EIGEN3_INCLUDE_DIR})
+  MESSAGE("EIGEN3_INCLUDE_DIR: " ${EIGEN3_INCLUDE_DIR})
   ADD_DEFINITIONS(-DCOMPILE_WITH_EIGEN)
 ENDIF()
 
