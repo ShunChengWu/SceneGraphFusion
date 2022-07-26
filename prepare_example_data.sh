5bf9017c00949aedca1430854240667b3fa06565
ShunChengWu <shuncheng.wu91@gmail.com>
2022-07-26 16:10:18 +0200

    Update prepare_example_data.sh
    update link

--- prepare_example_data.sh
+++ prepare_example_data.sh
@@ -1,4 +1,5 @@
 # This script is from https://github.com/WaldJohannaU/3RScan with MIT License (Copyright (c) 2020 Johanna Wald)
+# 26 July 2022, modified by Shun-Cheng Wu
 
 if [[ ! -d "data" ]]; then
 	mkdir data
@@ -7,14 +8,14 @@ fi
 # download example data
 if [[ ! -d "data/3RScan" ]]; then
 	if [[ ! -f "data/3RScan.v2.zip" ]]; then
-		wget "http://campar.in.tum.de/files/3RScan/3RScan.v2.zip" -P data
+		wget "http://www.campar.in.tum.de/public_datasets/3RScan/3RScan.v2.zip" -P data
 	fi
 	unzip "data/3RScan.v2.zip" -d ./data/3RScan
 fi
 
 if [[ ! -f "data/3RScan/3RScan.json" ]]; then
-	wget "http://campar.in.tum.de/files/3RScan/3RScan.json" -P data/3RScan
+	wget "http://www.campar.in.tum.de/public_datasets/3RScan/3RScan.json" -P data/3RScan
 fi
 if [[ ! -f "data/3RScan/objects.json" ]]; then
-	wget "http://campar.in.tum.de/files/3DSSG/3DSSG/objects.json" -P data/3RScan
+	wget "http://www.campar.in.tum.de/public_datasets/3DSSG/3DSSG/objects.json" -P data/3RScan
 fi
